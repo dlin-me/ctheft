29a46f17c19c12c1900f87b8a33426f58d8fcd4a
David Lin <david.lin.au@gmail.com>
2014-04-11 08:04:44 +1000

    try add command line

---
+++ bin/csteal.js
@@ -0,0 +1,10 @@
+#! /usr/bin/env node
+/**
+ * Created by davidlin on 11/04/2014.
+ */
+
+var ctheft = require('../lib/ctheft');
+
+var userArgs = process.argv.slice(2);
+var searchParam = userArgs[0];
+console.log(userArgs);
